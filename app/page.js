6ec7cb9a8efa48fb3f17a73fd8c4bdc25d9fe9c4
mizan21 <mizanorrahman398@gmail.com>
2024-08-27 12:01:35 +0600

    Define part added

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "./components/Home/Hero/Hero";
 import TimeSchbang from "./components/Home/TimeSchbang/TimeSchbang";
+import Define from "./components/Home/Define/Define";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <TimeSchbang />
+      <Define />
     </main>
   );
 }
