aed9d07f74cb7dbb91805c959d83a1d5a3acd68a
mizan21 <mizanorrahman398@gmail.com>
2024-09-18 14:23:03 +0600

    added

---
+++ app/about-details/page.js
@@ -0,0 +1,22 @@
+const page = () => {
+  return (
+    <div className="container mx-auto my-10">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
+        <div>
+          <img src="https://i.postimg.cc/P5XY7rvD/Whats-App-Image-2024-09-17-at-13-19-50.jpg"></img>
+        </div>
+        <div>
+          <img src="https://i.postimg.cc/G3DF8f14/Whats-App-Image-2024-09-17-at-13-19-51.jpg"></img>
+        </div>
+        <div>
+          <img src="https://i.postimg.cc/Dyd15b0F/Whats-App-Image-2024-09-17-at-13-19-51-1.jpg"></img>
+        </div>
+        <div>
+          <img src="https://i.postimg.cc/15ZppK9J/Whats-App-Image-2024-09-17-at-13-19-51-2.jpg"></img>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
