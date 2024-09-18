a25d8c8e945edc23c3eb5f277deb96155bbe770b
mizan21 <mizanorrahman398@gmail.com>
2024-09-18 14:14:10 +0600

    added

--- components/Solutions/BrandSolutions.jsx
+++ components/Solutions/BrandSolutions.jsx
@@ -29,47 +29,47 @@ const BrandSolutions = () => {
           <div className="mt-6 grid grid-cols-3 gap-4 mb-10">
             {/* Brand Logos */}
             <img
-              src="https://cdn.prod.website-files.com/64081b3f2fda69c80b5566e5/641d64fe6bc2e32b2eac9420_P_G-min-p-500.webp"
+              src="https://i.postimg.cc/XYnP8q4S/l-1.png"
               alt="P&G"
               className="h-10 object-contain mb-12"
             />
             <img
-              src="https://cdn.prod.website-files.com/64081b3f2fda69c80b5566e5/641d64faa61ee055ad1cc612_glow_and_lovely_logo%20-%20Prajna%20Anchan-min.png"
+              src="https://i.postimg.cc/VLqTnnyW/l-10.png"
               alt="Glow & Lovely"
               className="h-10 object-contain"
             />
             <img
-              src="https://cdn.prod.website-files.com/64081b3f2fda69c80b5566e5/641d64fbfeac4725f998b8de_GarnierMen_NewLogo%20(1)%20-%20Copy%20-%20Richa%20Nadkarni(1)-min-p-500.webp"
+              src="https://i.postimg.cc/28RM1Tt5/l-12.png"
               alt="Garnier Men"
               className="h-20 object-contain"
             />
             <img
-              src="https://cdn.prod.website-files.com/64081b3f2fda69c80b5566e5/646f68e1e441809161cd5207_download.webp"
+              src="https://i.postimg.cc/SQzHQHD5/l-13.png"
               alt="Kotak"
               className="h-10  mb-12"
             />
             <img
-              src="https://cdn.prod.website-files.com/64081b3f2fda69c80b5566e5/641d64f9f0752c75fb64ffce_jio-logo%20(1)-min.png"
+              src="https://i.postimg.cc/RhfYPnW9/l-14.png"
               alt="Jio"
               className="h-20 object-contain"
             />
             <img
-              src="https://cdn.prod.website-files.com/64081b3f2fda69c80b5566e5/64115e4b34e519f889ee3122_uno-logo.svg"
+              src="https://i.postimg.cc/76jcMNYm/l-17.png"
               alt="uno"
               className="h-16 object-contain"
             />
             <img
-              src="https://cdn.prod.website-files.com/64081b3f2fda69c80b5566e5/641d64fb1c4a4faaa6fd4a17_Godrej___Boyce-Logo.wine%20-%20Sadhana%20Yadav-min-p-500.webp"
+              src="https://i.postimg.cc/mDPJHnRP/l-18.png"
               alt="Godrej"
               className="h-16 object-contain mb-12"
             />
             <img
-              src="https://cdn.prod.website-files.com/64081b3f2fda69c80b5566e5/64115e4c3b3a8496bd204d10_AL_Logo.svg"
+              src="https://i.postimg.cc/HLZFjmGG/l-2.png"
               alt="Ashok Leyland"
               className="h-16 object-contain"
             />
             <img
-              src="https://cdn.prod.website-files.com/64081b3f2fda69c80b5566e5/641d5a3989b0aa7a4a169910_Dominos_Logo_png-min-p-500.png"
+              src="https://i.postimg.cc/BnCr9DMn/l-23.png"
               alt="domino's"
               className="h-16 object-contain"
             />
