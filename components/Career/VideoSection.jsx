e64daeccd8bdc64db064a68f22667e10c82c7f14
mizan21 <mizanorrahman398@gmail.com>
2024-09-26 15:28:40 +0600

    video part added

--- components/Career/VideoSection.jsx
+++ components/Career/VideoSection.jsx
@@ -3,7 +3,7 @@
 const VideoSection = () => {
   return (
     <div className="video-container w-full h-60 md:h-screen">
-      <iframe
+      {/* <iframe
         width="100%"
         height="100%"
         src="https://www.youtube.com/embed/CUTNgE4NPPg?si=vlqpjX_W9PXgUfr3"
@@ -12,7 +12,16 @@ const VideoSection = () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerpolicy="strict-origin-when-cross-origin"
         allowfullscreen
-      ></iframe>
+      ></iframe> */}
+      <video
+        className=" h-[100%] top-0 left-0 w-full md:h-full object-cover" // Fullscreen & responsive
+        autoPlay
+        loop
+        muted
+      >
+        <source src="/videos/career1.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };
