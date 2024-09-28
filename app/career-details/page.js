7197c3e24392b7048b76e03dcabf24976c16fd9c
mizan21 <mizanorrahman398@gmail.com>
2024-09-28 10:35:17 +0600

    Some part polishing

--- app/career-details/page.js
+++ app/career-details/page.js
@@ -13,8 +13,9 @@ const CareerDetails = () => {
           </h1>
           <p className="text-center max-w-[900px] text-white text-justify">
             We understand that finding a fulfilling career can be challenging.
-            Our mission is to simplify this process and offer a workplace you'll
-            enjoy coming to. Explore our job openings to start your journey.
+            Our mission is to simplify this process and offer a workplace
+            you&apos;ll enjoy coming to. Explore our job openings to start your
+            journey.
           </p>
           <div className="flex justify-center">
             <button className="text-black bg-white p-3 mt-4 ">
