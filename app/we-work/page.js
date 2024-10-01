84e5d3ad68b0166f3698dd16338cff5ff9c2d348
mizan21 <mizanorrahman398@gmail.com>
2024-10-01 13:25:13 +0600

    Some changes

---
+++ app/we-work/page.js
@@ -0,0 +1,12 @@
+import HowWeWorks from "@/components/About/HowWeWorks";
+import React from "react";
+
+const WeWork = () => {
+  return (
+    <div>
+      <HowWeWorks />
+    </div>
+  );
+};
+
+export default WeWork;
---
+++ components/About/HowWeWorks.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const HowWeWorks = () => {
+  return (
+    <div className="relative z-[110] bg-white pb-10 lg:pb-20">
+      <div className="text-justify px-[5%]">
+        <h1 className="text-3xl font-bold text-[#125B5C] py-[5%] lg:py-[95px] mb-8 md:text-3Xl lg:text-5xl">
+          Let&apos;s Make Your Brand Live. Choose Living Brands.
+        </h1>
+        <hr className="  w-full  text-gray-400" />
+        <div className=" ">
+          <div className="lg:flex lg:justify-between lg:items-start">
+            <div className="mt-5 lg:mt-16 ">
+              <h2 className="text-xl lg:text-3xl font-bold leading-[38px] text-black">
+                On A Mission to Showcase The Finest of Bangladeshi Creative
+                Talent to The World!
+              </h2>
+            </div>
+            <div className="mt-5 lg:mt-16  lg:pl-12">
+              <p className="text-lg text-gray-700 mb-3">
+                &quot;Living Brands&quot; embodies brands that evolve and grow,
+                continuously connecting with people. Just like life, a brand
+                must adapt and stay relevant. At Living Brands, we unite
+                strategy, creativity, and technology to craft brands that are
+                more than just seen—they are experienced.
+              </p>
+              <p className="text-lg text-gray-700 mb-3">
+                Our fully integrated marketing solutions bring together
+                creative, digital, and media services under one roof,
+                simplifying the client journey. We focus on building dynamic
+                brands that thrive across multiple channels, ensuring they adapt
+                to market changes and audience needs.
+              </p>
+              <p className="text-lg text-gray-700 mb-3">
+                With a holistic approach, we create brand ecosystems that live
+                and grow, offering solutions with sustained impact. Our unified
+                efforts ensure your brand isn&apos;t just relevant for today but
+                continues to evolve, creating lasting connections for the
+                future. At Living Brands, we don&apos;t just create campaigns—we
+                bring brands to life.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HowWeWorks;
