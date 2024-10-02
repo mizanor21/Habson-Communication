b832e0d6a14a05ac002dc5457aedee8b816fd0b6
mizan21 <mizanorrahman398@gmail.com>
2024-10-02 12:26:22 +0600

    Some updated

---
+++ app/who-we-are/page.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const WhoWeAre = () => {
+  return (
+    <div className="bg-white relative z-[110] grid grid-cols-1 lg:gap-y-20 py-10 lg:py-20">
+      <div className="grid gap-5 mb-8 sm:grid-cols-1 md:grid-cols-2 px-[5%]">
+        <div className="order-last md:order-first">
+          <h1 className="text-2xl font-bold mt-1 md:text-3Xl lg:text-5xl text-[#125b5c] mb-8">
+            Who We Are
+          </h1>
+          <p className="text-base  text-justify">
+            Living Brands is a dynamic digital marketing agency with a singular
+            mission: to turn your brand into a global powerhouse. We believe in
+            the strength of collaboration, where creative minds, marketing
+            specialists, and cutting-edge technology come together to craft
+            innovative strategies. With a relentless focus on results, we push
+            boundaries to ignite lasting growth and elevate your brand&apos;s
+            presence in the market.
+          </p>
+        </div>
+        <div className="order-first md:order-last flex justify-center items-center">
+          <video className="rounded-2xl lg:w-[800px]" autoPlay loop muted>
+            <source src="/videos/about1.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
+      <video
+        className="rounded-2xl lg:w-[100%] lg:h-[100%] px-[5%] "
+        autoPlay
+        loop
+        muted
+      >
+        <source src="/videos/about1.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+export default WhoWeAre;
