904f8efc9a3f1b63a0cfac163d227c3625fd1868
mizan21 <mizanorrahman398@gmail.com>
2024-10-03 17:23:15 +0600

    Some part added

--- app/meet-our-team/page.js
+++ app/meet-our-team/page.js
@@ -5,7 +5,7 @@ const page = () => {
     <div className=" relative z-[110] bg-white">
       <div className="flex justify-center my-20 ">
         <h2 className="text-2xl  md:text-5xl font-bold text-[#125b5c]">
-          Out Team
+          Our Team
         </h2>
       </div>
       <div className="mx-4 lg:mx-72">
